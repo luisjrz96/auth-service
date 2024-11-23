33c9683d197a863b6e995123857e305dc27c8865
Luis Juarez <luisjrz96dev@gmail.com>
2024-11-22 22:46:13 -0600

    updating the code according to spotless rules

--- src/test/java/com/luisjrz96/auth_service/AuthServiceApplicationTests.java
+++ src/test/java/com/luisjrz96/auth_service/AuthServiceApplicationTests.java
@@ -1,13 +1,6 @@
 package com.luisjrz96.auth_service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+class AuthServiceApplicationTests {}
